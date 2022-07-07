bf79c78fcdb05d0d7eebee401da311fe82ce31fc
Franky1 <arvid.teichtmann@gmx.de>
2022-07-07 23:31:29 +0200

    first sample app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import geopandas as gpd
 
 
 if __name__ == "__main__":
@@ -7,9 +8,15 @@ if __name__ == "__main__":
                     initial_sidebar_state='collapsed')
     st.title('🔨 Streamlit Template')
     st.markdown("""
-        This app is only a tempplate for a new Streamlit project. <br>
+        This app is only a template for a new Streamlit project. <br>
 
         ---
         """, unsafe_allow_html=True)
 
     st.balloons()
+    df = gpd.read_file('cities.geojson')
+    df['lon'] = df.geometry.x  # extract longitude from geometry
+    df['lat'] = df.geometry.y  # extract latitude from geometry
+    df = df[['lon','lat']]  # only keep longitude and latitude
+    st.write(df.head())  # show on table for testing only
+    st.map(df)  # show on map
